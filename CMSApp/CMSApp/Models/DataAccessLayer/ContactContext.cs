7513603cd8c7c39520ed816cd7bc81ad28175f32
Bikram <bikramchhetriat@gmail.com>
2018-07-02 09:18:32 +0530

    contextData changes.

--- CMSApp/CMSApp/Models/DataAccessLayer/ContactContext.cs
+++ CMSApp/CMSApp/Models/DataAccessLayer/ContactContext.cs
@@ -16,11 +16,11 @@ namespace CMSApp.Models.DataAccessLayer
             : base("name=ContactContext")
         {
         }
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            Database.SetInitializer<ContactContext>(null);
-            base.OnModelCreating(modelBuilder);
-        }
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    Database.SetInitializer<ContactContext>(null);
+        //    base.OnModelCreating(modelBuilder);
+        //}
 
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
